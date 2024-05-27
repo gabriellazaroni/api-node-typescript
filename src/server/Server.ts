66a6728e8ecc9ec7f685d6d4c54856d55e6cec30
Gabriel Augusto <gabriellazaroni@outlook.com.br>
2024-05-27 18:09:21 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,9 @@
- import express from 'express'
+import express from "express";
 
- const server = express()
+const server = express();
 
- server.get('/', (_, res) => {
-  return res.send('Hello, World!')
- })
+server.get("/", (_, res) => {
+  return res.send("Hello, World!");
+});
 
-
- export { server }
+export { server };
